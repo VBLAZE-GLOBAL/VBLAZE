42382eb0d05b4e7ed3ec8047adf4ef014d255ae3
Muhammed Minhaj Mahroof <72432356+minhaaj-t@users.noreply.github.com>
2024-11-21 23:52:39 +0400

    first commit 8

--- components/ClientLogos.tsx
+++ components/ClientLogos.tsx
@@ -35,7 +35,7 @@ export default function OurClients() {
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
                 {/* Image */}
-                <div className="relative">
+                <div className="relative flex justify-center items-center">
                   <Image
                     className="inline-flex"
                     src={ClientImg01}
@@ -66,7 +66,6 @@ export default function OurClients() {
                 </div>
               </div>
             </a>
-
             {/* Client 2 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
@@ -74,7 +73,7 @@ export default function OurClients() {
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
                 {/* Image */}
-                <div className="relative">
+                <div className="relative flex justify-center items-center">
                   <Image
                     className="inline-flex"
                     src={ClientImg02}
@@ -105,7 +104,6 @@ export default function OurClients() {
                 </div>
               </div>
             </a>
-
             {/* Client 3 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
@@ -113,7 +111,7 @@ export default function OurClients() {
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
                 {/* Image */}
-                <div className="relative">
+                <div className="relative flex justify-center items-center">
                   <Image
                     className="inline-flex"
                     src={ClientImg03}
@@ -144,26 +142,26 @@ export default function OurClients() {
                 </div>
               </div>
             </a>
-
-            {/* Client 4 */}
+            {/* Additional clients */}
+            {/* Client 3 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
-              href="https://www.savvypoolservices.com/"
+              href="https://savvypool.com/"
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
                 {/* Image */}
-                <div className="relative">
+                <div className="relative flex justify-center items-center">
                   <Image
                     className="inline-flex"
                     src={ClientImg04}
                     width={350}
                     height={288}
-                    alt="Client 04"
+                    alt="Client 03"
                   />
                   {/* Client name on hover */}
                   <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     <span className="text-white text-xl font-semibold">
-                      Savvy Pool
+                      TRADING
                     </span>
                   </div>
                 </div>
@@ -172,19 +170,18 @@ export default function OurClients() {
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
                       <span className="bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent">
-                        Pool Care Experts
+                        Effortless Trading
                       </span>
                     </span>
                   </div>
                   <p className="text-indigo-200/65">
-                    Providing professional pool maintenance and cleaning
-                    services for a sparkling experience.
+                    Empowering investors with intuitive platforms and expert
+                    insights that simplify complex markets.
                   </p>
                 </div>
               </div>
             </a>
-
-            {/* Client 4 */}
+            {/* Client 3 */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
               href="https://savvypool.com/"
@@ -197,7 +194,7 @@ export default function OurClients() {
                     src={ClientImg05}
                     width={350}
                     height={288}
-                    alt="Client 04"
+                    alt="Client 03"
                   />
                   {/* Client name on hover */}
                   <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
