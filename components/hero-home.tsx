4a68769c33e652a5a554f3afab168327ea4e6dc9
Muhammed Minhaj Mahroof <72432356+minhaaj-t@users.noreply.github.com>
2024-11-22 14:14:57 +0400

    Resolved merge conflicts v3.7

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -24,7 +24,7 @@ export default function HeroHome() {
                 INNOVATE. CODE. DELIVER
               </p>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
-                <div data-aos="fade-up" data-aos-delay={400}>
+                {/* <div data-aos="fade-up" data-aos-delay={400}>
                   <a
                     className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
                     href="#0"
@@ -36,11 +36,11 @@ export default function HeroHome() {
                       </span>
                     </span>
                   </a>
-                </div>
+                </div> */}
                 <div data-aos="fade-up" data-aos-delay={600}>
                   <a
                     className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-                    href="/signin"
+                    href="/schedule"
                   >
                     Schedule a Meet
                   </a>
