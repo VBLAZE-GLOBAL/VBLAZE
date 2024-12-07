d299518e19f68b34448c354492e17b51f40a40a8
Muhammed Minhaj Mahroof <72432356+minhaaj-t@users.noreply.github.com>
2024-12-07 16:01:26 +0400

    Resolved merge conflicts v11.74

--- app/(default)/sitemap.ts
+++ app/(default)/sitemap.ts
@@ -1,3 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader("Content-Type", "application/xml");
+  const sitemapXml = `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
@@ -75,3 +80,6 @@
 
 
 </urlset>
+    `;
+  res.status(200).send(sitemapXml);
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/", // This points to your dynamic sitemap API route
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
