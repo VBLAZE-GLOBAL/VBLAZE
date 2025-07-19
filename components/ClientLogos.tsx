34a8d5c4b8b5f7ebea078e86795854bae0269ccb
Muhammed Minhaj Mahroof <mminhajmahroof@gmail.com>
2025-07-19 11:00:12 +0400

    SEO Updates tag 3

--- components/ClientLogos.tsx
+++ components/ClientLogos.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
-import ClientImg01 from "@/public/images/client-10.png";
-import ClientImg02 from "@/public/images/client-02.png";
-import ClientImg03 from "@/public/images/client-11.png";
-import ClientImg04 from "@/public/images/client-04.png";
-import ClientImg05 from "@/public/images/client-12.png";
+import ClientImg07 from "@/public/images/client-07.png";
+import ClientImg01 from "@/public/images/client-01.png";
+import ClientImg13 from "@/public/images/client-13.png";
 import Spotlight from "@/components/spotlight";
 
 export default function OurClients() {
@@ -27,26 +25,28 @@ export default function OurClients() {
             </p>
           </div>
           {/* Spotlight items */}
-          <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-5 md:grid-cols-2">
-            {/* Client 2 */}
+          <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-3 md:grid-cols-2">
+            {/* Media Market International */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
-              href="https://stthomaskannur.ac.in/fiesta23/"
+              href="https://www.mediamarketint.com/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
                 {/* Image */}
                 <div className="relative pt-3 flex justify-center items-center">
                   <Image
                     className="inline-flex"
-                    src={ClientImg02}
+                    src={ClientImg13}
                     width={350}
                     height={288}
-                    alt="Client 02"
+                    alt="Media Market International"
                   />
                   {/* Client name on hover */}
                   <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     <span className="text-white text-xl font-semibold">
-                      FIESTA
+                      Media Market International
                     </span>
                   </div>
                 </div>
@@ -55,35 +55,35 @@ export default function OurClients() {
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
                       <span className="bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent">
-                        Seamless Event
+                        Global Media & Marketing
                       </span>
                     </span>
                   </div>
                   <p className="text-indigo-200/65">
-                  Shaping Tomorrows Tech Leaders Through Insight, Innovation & Invention
+                    Media Market International is a leading provider of media and marketing solutions, delivering innovative strategies for global clients.
                   </p>
                 </div>
               </div>
             </a>
-            {/* Client 3 */}
+            {/* SavvyPool */}
             <a
               className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
-              href="http://ayenz.com/trading/"
+              href="/projects/savvypool"
             >
               <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
                 {/* Image */}
-                <div className="relative flex pt-3 justify-center items-center">
+                <div className="relative pt-3 flex justify-center items-center">
                   <Image
                     className="inline-flex"
-                    src={ClientImg04}
+                    src={ClientImg07}
                     width={350}
                     height={288}
-                    alt="Client 03"
+                    alt="SavvyPool"
                   />
                   {/* Client name on hover */}
                   <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     <span className="text-white text-xl font-semibold">
-                      TRADING
+                      SavvyPool
                     </span>
                   </div>
                 </div>
@@ -92,12 +92,49 @@ export default function OurClients() {
                   <div className="mb-3">
                     <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
                       <span className="bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent">
-                        Effortless Trading
+                        Learning Management
                       </span>
                     </span>
                   </div>
                   <p className="text-indigo-200/65">
-                  Delivering Precision, Quality & Sustainable Building Solutions Across the UAE.
+                    A comprehensive LMS app offering personalized LMS and real-time tracking.
+                  </p>
+                </div>
+              </div>
+            </a>
+            {/* Jovoy */}
+            <a
+              className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
+              href="/projects/jovoy"
+            >
+              <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
+                {/* Image */}
+                <div className="relative pt-3 flex justify-center items-center">
+                  <Image
+                    className="inline-flex"
+                    src={ClientImg01}
+                    width={350}
+                    height={288}
+                    alt="Jovoy"
+                  />
+                  {/* Client name on hover */}
+                  <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <span className="text-white text-xl font-semibold">
+                      Jovoy
+                    </span>
+                  </div>
+                </div>
+                {/* Content */}
+                <div className="p-6">
+                  <div className="mb-3">
+                    <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
+                      <span className="bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent">
+                        Perfume Metrics Platform
+                      </span>
+                    </span>
+                  </div>
+                  <p className="text-indigo-200/65">
+                    An all-in-one platform for tracking perfume metrics and building better habits.
                   </p>
                 </div>
               </div>
@@ -105,7 +142,7 @@ export default function OurClients() {
           </Spotlight>
           <a
             className="group relative mx-auto mt-12 flex h-12 w-48 items-center justify-center overflow-hidden rounded-full border border-gradient-to-r border-indigo-500 text-center transition-all duration-300 hover:scale-105"
-            href="/projects" // Adjust the href to the desired link
+            href="/projects"
           >
             <span className="relative z-10 text-lg font-medium text-indigo-500 transition-all duration-300 group-hover:text-indigo-400">
               MORE PROJECTS
